2913b76ecee2b63ac845d25aa008f5303916d4d6
Roc-J <646481338@qq.com>
2017-03-03 21:55:42 +0800

    First program by qjk

---
+++ Chapter3/src/myclass/struct/First.java
@@ -0,0 +1,13 @@
+package myclass.struct;
+
+public class First {
+	private static String s1="我就想知道,";
+	
+	public static void main(String[] args) {
+		String s2="明天是不是周末";
+		System.out.print(s1);
+		System.out.println(s2);
+
+	}
+
+}
