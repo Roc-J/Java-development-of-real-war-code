102b8d0e9bc23c8d0518151c2921ab2fc46299da
Roc-J <646481338@qq.com>
2017-03-03 20:52:53 +0800

    the second program

---
+++ Chapter2/HelloJava/src/StudyJava.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class StudyJava {
+	private static Date now = new Date();
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("从 "+ now +"开始，我就要学习java编程了~");
+	}
+
+}
